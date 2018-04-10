276f94dbab90a7d775d730d7e9259a6520e070d7
Liam Poling <lcpoling3@gmail.com>
2018-04-09 23:44:39 -0400

    basic player movement/camera rotation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,14 +6,18 @@ using UnityEngine.UI;
 public class PlayerMovement : MonoBehaviour {
     private Rigidbody rb;
 
+
     public float moveSpeed = 5F;
     public float rotSpeed = 10f;
     public float jumpSpeed = 3f;
     float angle;
+    float rotX;
+    float rotY;
 
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody>();
+
 	}
 	
 	// Update is called once per frame
@@ -22,27 +26,17 @@ public class PlayerMovement : MonoBehaviour {
         float moveHorizonal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 mousePos = Input.mousePosition;
+        rotX += Input.GetAxis("Mouse X");
+        rotY -= Input.GetAxis("Mouse Y");
+    
+        transform.eulerAngles = new Vector3(0, rotX, 0);
 
-
-        //To make mousePos relative to center of screen
-        mousePos.x -= Screen.width / 2;
-        mousePos.y -= Screen.height / 2;
-
-        //To make mousePos relative to transform
-        mousePos += transform.position;
-        angle = Vector3.Angle(mousePos, Vector3.right);
-
-
-        transform.rotation = Quaternion.Euler(angle, 0, 0);
-        
-
-        Vector3 movement = new Vector3(0.0f, 0.0f, -moveVertical);
-
+        Vector3 movementV = new Vector3(0.0f, 0.0f, -moveVertical);
+        Vector3 movementH = new Vector3(-moveHorizonal, 0.0f, 0.0f);
         Vector3 up = new Vector3(0.0f, 1f, 0.0f);
-        
-        transform.Translate(movement * moveSpeed * Time.deltaTime);
- 
+
+        transform.Translate(movementV * moveSpeed * Time.deltaTime);
+        transform.Translate(movementH * moveSpeed * Time.deltaTime);
 
         if (Input.GetKeyDown("space"))
         {
