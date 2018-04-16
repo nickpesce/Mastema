dd4c053664c40bf9fc8859acea12101922303b14
nickpesce <nickpesce22@gmail.com>
2018-04-16 12:06:41 -0400

    Changed player movement to allow for more physics

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,55 +1,71 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
+using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 10.0f;
+    public float gravity = 10.0f;
+    public float maxVelocityChange = 10.0f;
+    public bool canJump = true;
+    public float jumpHeight = 2.0f;
+    private bool grounded = false;
+    GameObject ground;
     private Rigidbody rb;
 
-
-    public float moveSpeed = 5F;
-    public float rotSpeed = 10f;
-    public float jumpSpeed = 3f;
-    float angle;
-    float rotX;
-    float rotY;
-
-    // Use this for initialization
-    void Start () {
+    void Awake()
+    {
         rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        rb.useGravity = false;
+    }
 
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-
-        float moveHorizonal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        //rotX += Input.GetAxis("Mouse X");
-        //rotY -= Input.GetAxis("Mouse Y");
-
-        //transform.eulerAngles = new Vector3(0, rotX, 0);
-
-
-        Vector3 movementV = new Vector3(0.0f, 0.0f, moveVertical);
-        Vector3 movementH = new Vector3(-moveHorizonal, 0.0f, 0.0f);
-        Vector3 up = new Vector3(0.0f, 1f, 0.0f);
-
-        transform.Translate(movementV * moveSpeed * Time.deltaTime);
-        transform.Translate(movementH * moveSpeed * Time.deltaTime);
-
-        if (Input.GetKeyDown("space"))
+    void FixedUpdate()
+    {
+        if (grounded)
         {
+            // Lower friction, lower self-propelled acceleration.
+            float maxVelocityChangeWithFriction = maxVelocityChange * ground.GetComponent<Collider>().material.dynamicFriction;
+            // Calculate how fast we should be moving
+            Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            targetVelocity = transform.TransformDirection(targetVelocity);
+            targetVelocity *= speed;
 
-            if (transform.position.y < 5 && transform.position.y >= 1)
+            // Apply a force that attempts to reach our target velocity
+            Vector3 velocity = rb.velocity;
+            Vector3 velocityChange = (targetVelocity - velocity);
+            velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChangeWithFriction, maxVelocityChangeWithFriction);
+            velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChangeWithFriction, maxVelocityChangeWithFriction);
+            velocityChange.y = 0;
+            rb.AddForce(velocityChange, ForceMode.VelocityChange);
+
+            // Jump
+            if (canJump && Input.GetButton("Jump"))
             {
-                rb.AddForce(up * jumpSpeed, ForceMode.Impulse);
+                rb.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
             }
         }
 
+        // We apply gravity manually for more tuning control
+        rb.AddForce(new Vector3(0, -gravity * rb.mass, 0));
+
+        grounded = false;
+    }
+
+    void OnCollisionStay(Collision hit)
+    {
+        grounded = true;
+        ground = hit.gameObject;
+    }
 
 
-
+    float CalculateJumpVerticalSpeed()
+    {
+        // From the jump height and gravity we deduce the upwards speed 
+        // for the character to reach at the apex.
+        return Mathf.Sqrt(2 * jumpHeight * gravity);
     }
 }
