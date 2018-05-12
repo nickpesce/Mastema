4429e9b33b13fd944705a72a4e5fbce969408aa4
Liam Poling <lcpoling3@gmail.com>
2018-05-12 19:41:18 -0400

    added item spawn weighting

--- Assets/Scripts/Floor/ItemGenerator.cs
+++ Assets/Scripts/Floor/ItemGenerator.cs
@@ -9,19 +9,48 @@ public class ItemGenerator : NetworkBehaviour {
     public float spawnChancePerUpdate = .01f;
     FloorController floorController;
 
+    private int totalWeight = 0;
+    private int[] weights;
+
+
     void Start()
     {
         floorController = GetComponent<FloorController>();
 
+        weights = new int[Item.allItems.Count];
+
+        for (int i = 0; i < Item.allItems.Count; i++)
+        {
+            totalWeight += Item.allItems[i].GetWeight();
+        }
+
+        weights[0] = Item.allItems[0].GetWeight();
+        for (int i = 1; i < Item.allItems.Count; i++)
+        {
+            weights[i] = weights[i - 1] + Item.allItems[i].GetWeight();
+        }
     }
 
     [ServerCallback]
     void FixedUpdate()
     {
+        
         if (Random.value < spawnChancePerUpdate)
         {
+            int pickWeight = Random.Range(0, totalWeight);
+            int itemToGen = 0;
+            for (int i = 0; i < weights.Length; i++)
+            {
+                if (pickWeight < weights[i])
+                {
+                    itemToGen = i;
+                    break;
+                }
+            }
+
+           
             Vector3 spawnLocation = floorController.GetRandomPosition();
-            Item.SpawnOnGround(Random.Range(0, Item.allItems.Count), spawnLocation);
+            Item.SpawnOnGround(itemToGen, spawnLocation);
         }
     }
 
